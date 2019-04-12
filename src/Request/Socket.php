ef00c443e1450c8047d646f30c9b8f1bedc6ddda
omer <qmerfp@me.com>
2019-04-12 18:36:49 +0300

    error exception

--- src/Request/Socket.php
+++ src/Request/Socket.php
@@ -14,10 +14,14 @@ class Socket
      */
     static function get($domain)
     {
-        $g = stream_context_create(array("ssl" => array("capture_peer_cert" => true)));
-        $r = stream_socket_client("ssl://{$domain}:443", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $g);
-        $cont = stream_context_get_params($r);
-        return openssl_x509_parse($cont["options"]["ssl"]["peer_certificate"]);
 
+        try {
+            $g = stream_context_create(array("ssl" => array("capture_peer_cert" => true)));
+            $r = stream_socket_client("ssl://{$domain}:443", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $g);
+            $cont = stream_context_get_params($r);
+            return openssl_x509_parse($cont["options"]["ssl"]["peer_certificate"]);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
     }
 }
